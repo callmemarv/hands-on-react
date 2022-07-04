70c27324a51a7d7787cf3e41269ba9613a562c1a
Marvin Therolf <marvin.therolf@qaware.de>
2022-07-04 10:32:16 +0200

    Implement hello world component

---
+++ src/component/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HelloWorld(): JSX.Element {
+    const content = 'Hello world'
+
+    return (
+        <p>{content}</p>
+    )
+}
+
+export default HelloWorld
